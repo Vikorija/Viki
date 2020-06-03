579048636dd7c1fd73b4e97d57bb0ef4718b8424
Vikorija <39631741+Vikorija@users.noreply.github.com>
2020-06-03 15:08:10 +0300

    1.

--- src/test/java/Hw16.java
+++ src/test/java/Hw16.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class Hw16 {
 
@@ -14,16 +16,17 @@ public class Hw16 {
         driver.manage().window().maximize();
         driver.get("https://hotline.ua/");
 
-               driver.findElement(By.id("searchbox")).sendKeys("ноутбук");
+        driver.findElement(By.id("searchbox")).sendKeys("ноутбук");
         driver.findElement(By.id("doSearch")).click();
-        WebDriverWait wait = new WebDriverWait(driver);
-        driver.findElement(By.xpath("//*[@id='page-catalog']/div[1]/div[1]/div[2]/div[2]/div/div[3]/div/div[2]/ul/li[4]/label")).click();
+
+
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //driver.findElement(By.xpath("//*[@id=\"page-catalog\"]/div[1]/div[1]/div[2]/aside/div/div[3]/div[1]/div[2]/div[2]/ul/li[4]/label/i")).click();
 
         String titleOfThePage = driver.getTitle();
-        System.out.println( "Title of the page:" + titleOfThePage);
+        System.out.println("Title of the page:" + titleOfThePage);
 
     }
-
 
 
 }
